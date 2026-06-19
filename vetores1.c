cf7b04df167f3aae2816f8f2245794ac1463b47b
BetaniaAraujo <mariabetania.nazareth@gmail.com>
2026-06-19 18:04:24 +0100

    training

--- vetores1.c
+++ vetores1.c
@@ -4,25 +4,54 @@
 
 int main() 
 {   
-    int num1[10], num2[8], soma = 0;
-    //1-Leitura e apresentação de valores
-    //Lê 10 números inteiros para um vetor.
-    /*for(int i = 0; i < 10; i++ ) {
-        printf("Digite um numero: ");
-        scanf("%d", &num1[i]);
-    }//Mostra todos os valores introduzidos.
+    int vet1[10], vet2[8], soma = 0, vet3[10], maior, posmaior;
+//1-Leitura e apresentação de valores
+//Lê 10 números inteiros para um vetor.
+    /*for(int i = 0; i < 10; i++) {
+        printf("Digite um numero %dº: ", i + 1);
+        scanf("%d", &vet1[i]);
+    }
+//Mostra todos os valores introduzidos.
     for(int i = 0; i < 10; i++ ) {
-        printf(" %d | ", num1[i]);
+        printf(" %d | ", vet1[i]);
     }
-    printf("\n");*/
-    //2- Soma dos elementos
-    //Lê 8 números inteiros para um segundo vetor.
+    printf("\n");
+//2- Soma dos elementos
+//Lê 8 números inteiros para um segundo vetor.
     for(int i = 0; i < 8; i++ ) {
-        printf("Digite um numero: ");
-        scanf("%d", &num2[i]);
-        soma = soma + num2[i];
+        printf("Digite um numero %dº: ", i + 1);
+        scanf("%d", &vet2[i]);
+//Calcula e apresenta a soma de todos os elementos.    
+        soma = soma + vet1[i] + vet2[i];
     }
-
+//Mostra todos os valores introduzidos.
+    for(int i = 0; i < 8; i++ ) {
+        printf(" %d | ", vet2[i]);
+    }printf("\n");
+    printf("A soma de todos os elementos e: %d ", soma);*/
+//3- Maior e menor valor
+//Lê 10 números inteiros para um terceiro vetor.
+    for(int i = 0; i < 10; i++ ) {
+        printf("Digite um número %dº: ", i + 1);
+        scanf("%d", &vet3[i]);
+    }
+//Mostra todos os valores introduzidos.
+    for(int i = 0; i < 10; i++ ) {
+        printf(" %d | ", vet3[i]);
+    }
+    printf("\n");
+    maior = vet3[0];
+    posmaior = vet3[0];
+    for(int i = 0; i < 10; i++) {
+        if(vet3[i] > maior) {
+            maior = vet3[i];
+            posmaior = vet3[i];
+        }
+    }
+    
+    
+    printf("O maior valor e: %d ", maior);
+    printf("A posicao do maior valor e: %d ", posmaior + 1);
 
 return 0;
 }
