3ce2fa46455e97ed9e61aeddc2c8ff6b1121b9fa
BetaniaAraujo <mariabetania.nazareth@gmail.com>
2026-06-29 18:16:29 +0100

    training

--- matriz1a.c
+++ matriz1a.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int linhas, colunas, soma = 0;
+    int linhas, colunas, soma = 0, maior = 0, poslinhamaior = 0, poscolmaior = 0, somadiagonal = 0, par;
     printf("Insira o numero de linhas: ");
     scanf("%d", &linhas);
     printf("Insira o numero de colunas: ");
@@ -13,7 +13,10 @@ int main() {
         for(int j = 0; j < colunas; j++) {            
             printf("Insira dados da posicao: linha %d coluna %d: ", (i+1), (j+1));
             scanf("%d", &matriz[i][j]);
-            soma = soma + matriz[i][j]; //Calcula e apresenta a soma de todos os seus elementos. 
+            soma = soma + matriz[i][j]; //Calcula a soma de todos os seus elementos.
+            if(matriz[i][j] %2 == 0) {
+                par += 1;
+            } 
         }
     }
     printf("\nImprimindo a matriz\n");
@@ -25,7 +28,39 @@ int main() {
         }
     }
     printf("\n\n");
-    printf("Soma dos %d os elementos e de: %d", (linhas * colunas), soma);
+    printf("Soma dos %d os elementos e de: %d", (linhas * colunas), soma); //apresenta a soma de todos os seus elementos.
 
+    for(int i = 0; i < linhas; i++){
+        printf("\n");
+        for(int j = 0; j < colunas; j++) {            
+            printf("linha %d | coluna %d = %d ", i, j, matriz[i][j]);   
+        }
+    }
+    maior = matriz[0][0];
+    for(int i = 0; i < linhas; i++) {
+        for(int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+            if(matriz[i][j] > maior) {
+             maior = matriz[i][j];
+             poslinhamaior = i;
+             poscolmaior = j;   
+            }          
+            printf("O maior valor existente na matriz é: %d, que esta na linha %d e coluna %d ", matriz, poslinhamaior+1, poscolmaior+1);
+            printf("Existem %d numeros pares nessa matriz. ", par);
+        }
+    }
+    for(int i = 0; i < linhas; i++) {
+        for(int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+            if(i == j) {
+             printf("%d", matriz[i][j]);
+             somadiagonal += matriz[i][j];
+            }
+            else {
+                printf(" ");
+            }
+            printf("\n");       
+        }
+    }       
     return 0;
 }
