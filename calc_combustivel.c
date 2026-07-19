93714243966d52db74650354c4446b06ee6f5da0
BetaniaAraujo <mariabetania.nazareth@gmail.com>
2026-07-19 21:01:48 +0100

    training

---
+++ calc_combustivel.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//Quanto custa encher o tanque de um carro que tem 50 litros de capacidade, está com 20 litros de combustível atualmente e o custo do combustível é de R$5,80/litro?
+
+int main() {
+
+    int CapTanque, VolAtual; LitrosCompl
+    float Combustivel = 5.80, CustoTotal;
+//Descobrir quantos litros faltam para encher o tanque
+    printf("Qual a capacidade do tanque: ");
+    scanf("%d", &CapTanque);
+    printf("Quantos litros ha no tanque: ");
+    scanf("%d", &VolAtual);
+
+    LitrosCompl = CapTanque - VolAtual;
+    scanf("%d", &LitrosCompl);
+    printf("Faltam %d litros para completar o tanque", LitrosCompl);
+//Pegar o custo de 1 litro e multiplicar pelo resultado do passo 1
+        
+
+    printf("O custo para completar o tanque e: ", CustoTotal )
+
+    return 0;
+}
